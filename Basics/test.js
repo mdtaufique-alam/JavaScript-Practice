e81d38fc223c8730411267d0b9856a4131d775c8
Md Taufique Alam <iam.shamstaufique@gmail.com>
2025-09-14 13:32:16 +0530

    math and nums

--- Basics/test.js
+++ Basics/test.js
@@ -1,1 +1,25 @@
 console.log("Hello World");
+
+
+const name = "Taufique"
+const repoCount = 5
+
+// console.log(name + repoCount + "Value");
+
+// console.log('Hello my name is ${name.} and my repo count is ${repoCount}');
+
+const gameName = new String('Taufique')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+console.log(gameName.indexOf('u'));
+
+ 
+
+
+
+
