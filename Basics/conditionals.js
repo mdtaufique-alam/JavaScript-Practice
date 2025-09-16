f88604ea23491144c8a8f28f946a828f01b7b903
Md Taufique Alam <iam.shamstaufique@gmail.com>
2025-09-16 23:59:25 +0530

    conditionals

---
+++ Basics/conditionals.js
@@ -0,0 +1,19 @@
+// const age = 55;
+
+
+// if(age >= 50) {
+//     console.log('No, you are an adult')
+// }
+// else if (age>= 18) {
+//     console.log('Yes, you can vote')
+// } else {
+//     console.log('No, you cant vote');
+    
+// }
+    
+
+//  Ternary Operator
+const age = 50;
+let bat = '50';
+age === bat ? console.log('yes') : console.log('NO');
+
